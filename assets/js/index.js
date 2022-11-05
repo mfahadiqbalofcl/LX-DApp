beeeddbbe4c977f80518e757bfd1825d2c3185fb
M Fahad Iqbal <74042506+MFahadiqbalofcl@users.noreply.github.com>
2022-11-05 12:10:38 -0700

    timer disabled
    i have disabled the countdown timer in this version

--- assets/js/index.js
+++ assets/js/index.js
@@ -150,31 +150,6 @@ async function unstake() {
   }
 }
 
-// Countdown Timer
-let deadline = new Date('Oct 5, 2023 15:37:25').getTime()
-
-let x = setInterval(function () {
-  let now = new Date().getTime()
-  let t = deadline - now
-  let days = Math.floor(t / (1000 * 60 * 60 * 24))
-  let hours = Math.floor((t % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
-  let minutes = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60))
-  let seconds = Math.floor((t % (1000 * 60)) / 1000)
-  document.getElementById('day').innerHTML = days
-  document.getElementById('hour').innerHTML = hours
-  document.getElementById('minute').innerHTML = minutes
-  document.getElementById('second').innerHTML = seconds
-  if (t < 0) {
-    clearInterval(x)
-    document.getElementById('countdown').innerHTML = '(Expired)'
-    document.getElementById('countdown').style.color = 'red'
-    document.getElementById('day').innerHTML = '0'
-    document.getElementById('hour').innerHTML = '0'
-    document.getElementById('minute').innerHTML = '0'
-    document.getElementById('second').innerHTML = '0'
-  }
-})
-
 // Disable Developer Tools
 document.addEventListener('keydown', function () {
   if (event.keyCode == 123) {
