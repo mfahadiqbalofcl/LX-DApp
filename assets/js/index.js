54872ff2cd136d9c9e6ead9d475db7fda0d7dfe1
M Fahad Iqbal <74042506+MFahadiqbalofcl@users.noreply.github.com>
2022-11-07 07:57:28 -0800

    enabled the dev tools

--- assets/js/index.js
+++ assets/js/index.js
@@ -151,46 +151,46 @@ async function unstake() {
 }
 
 // Disable Developer Tools
-document.addEventListener('keydown', function () {
-  if (event.keyCode == 123) {
-    alert("This function has been disabled to view the code!");
-    return false;
-  } else if (event.ctrlKey && event.shiftKey && event.keyCode == 73) {
-    alert("This function has been disabled to view the code!");
-    return false;
-  } else if (event.ctrlKey && event.keyCode == 85) {
-    alert("This function has been disabled to view the code!");
-    return false;
-  }
-}, false);
+// document.addEventListener('keydown', function () {
+//   if (event.keyCode == 123) {
+//     alert("This function has been disabled to view the code!");
+//     return false;
+//   } else if (event.ctrlKey && event.shiftKey && event.keyCode == 73) {
+//     alert("This function has been disabled to view the code!");
+//     return false;
+//   } else if (event.ctrlKey && event.keyCode == 85) {
+//     alert("This function has been disabled to view the code!");
+//     return false;
+//   }
+// }, false);
 
-if (document.addEventListener) {
-  document.addEventListener('contextmenu', function (e) {
-    alert("This function has been disabled to view the code!");
-    e.preventDefault();
-  }, false);
-} else {
-  document.attachEvent('oncontextmenu', function () {
-    alert("This function has been disabled to view the code!");
-    window.event.returnValue = false;
-  });
-}
+// if (document.addEventListener) {
+//   document.addEventListener('contextmenu', function (e) {
+//     alert("This function has been disabled to view the code!");
+//     e.preventDefault();
+//   }, false);
+// } else {
+//   document.attachEvent('oncontextmenu', function () {
+//     alert("This function has been disabled to view the code!");
+//     window.event.returnValue = false;
+//   });
+// }
 
-document.addEventListener('contextmenu', event => event.preventDefault());
-document.onkeydown = function (e) {
-  if (event.keyCode == 123) {
-    return false;
-  }
-  if (e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
-    return false;
-  }
-  if (e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
-    return false;
-  }
-  if (e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
-    return false;
-  }
-}
+// document.addEventListener('contextmenu', event => event.preventDefault());
+// document.onkeydown = function (e) {
+//   if (event.keyCode == 123) {
+//     return false;
+//   }
+//   if (e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
+//     return false;
+//   }
+//   if (e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
+//     return false;
+//   }
+//   if (e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
+//     return false;
+//   }
+// }
 
 $(document).ready(function () {
   /*	Disables mobile keyboard from displaying when clicking +/- inputs */
