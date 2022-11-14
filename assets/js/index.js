4661c024f36ef3a734b29402fc3e80fb0fe7cf17
M Fahad Iqbal <74042506+mfahadiqbalofcl@users.noreply.github.com>
2022-11-14 15:42:48 -0800

    Update index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -32,7 +32,6 @@ window.onload = function () {
   setTimeout(function () {
     preLoader.style.display = 'none'
     document.body.style.overflow = 'auto'
-    confirm('This app is just for demo purposes')
   })
 }
 
